3806dac753bf22cf452465822d746277416559a7
httruc <hoangthuytruc@gmail.com>
2023-12-28 16:51:20 +0700

    polish code

--- Lab01/Blockchain.go
+++ Lab01/Blockchain.go
@@ -1,17 +1,11 @@
 package Lab01
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"encoding/binary"
-	"encoding/gob"
 	"encoding/hex"
 	"fmt"
 	"github.com/dgraph-io/badger"
-	"log"
 	"os"
 	"runtime"
-	"time"
 )
 
 const (
@@ -94,54 +88,6 @@ func InitBlockchain(address string) *Blockchain {
 	Handle(err)
 	blockchain := Blockchain{lastHash, db}
 	return &blockchain
-}
-
-type Block struct {
-	Timestamp     int64
-	Transactions  []*Transaction
-	PrevBlockHash []byte
-	Hash          []byte
-	Nonce         int
-}
-
-func Int64ToBytes(n int64) []byte {
-	b := make([]byte, 8)
-	binary.LittleEndian.PutUint64(b, uint64(n))
-	return b
-}
-
-func (b *Block) HashTransactions() []byte {
-	var txHashes [][]byte
-
-	for _, tx := range b.Transactions {
-		txHashes = append(txHashes, tx.Serialize())
-	}
-	tree := NewMerkleTree(txHashes)
-
-	return tree.RootNode.Data
-}
-
-func (b *Block) SetHash() {
-	data := bytes.Join([][]byte{b.PrevBlockHash, b.HashTransactions(), Int64ToBytes(b.Timestamp)}, []byte{})
-	hash := sha256.Sum256(data)
-	b.Hash = hash[:]
-}
-
-func CreateBlock(txs []*Transaction, prevHash []byte) *Block {
-	block := &Block{
-		time.Now().UnixNano(),
-		txs,
-		prevHash,
-		[]byte{},
-		0,
-	}
-	pow := NewProof(block)
-	nonce, hash := pow.Run()
-
-	block.Nonce = nonce
-	block.Hash = hash
-
-	return block
 }
 
 func (blockchain *Blockchain) AddBlock(txs []*Transaction) {
@@ -273,26 +219,4 @@ Work:
 	}
 
 	return accumulated, unspentOuts
-}
-
-func (b *Block) Serialize() []byte {
-	var res bytes.Buffer
-	encoder := gob.NewEncoder(&res)
-	err := encoder.Encode(b)
-	Handle(err)
-	return res.Bytes()
-}
-
-func Deserialize(data []byte) *Block {
-	var block Block
-	decoder := gob.NewDecoder(bytes.NewReader(data))
-	err := decoder.Decode(&block)
-	Handle(err)
-	return &block
-}
-
-func Handle(err error) {
-	if err != nil {
-		log.Panicln(err)
-	}
 }
