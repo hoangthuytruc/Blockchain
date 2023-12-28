6898e90ad29f70621a8f54abd37473e1a7716f96
VinhThuyen <VinhThuyen7494@gmail.com>
2023-12-28 20:45:29 +0700

    * upgrade Merkle Tree code to handle verify node & transaction * add test scenario for merkle tree in commandline

--- Lab01/merkle.go
+++ Lab01/merkle.go
@@ -1,26 +1,56 @@
 package Lab01
 
-import "crypto/sha256"
+import (
+	"bytes"
+	"crypto/sha256"
+	"errors"
+	"hash"
+)
 
+// hash region
+var DefaultShaHasher = NewHasher(sha256.New)
+
+type HashFn func() hash.Hash
+type Hasher struct{ Imp HashFn }
+
+func NewHasher(imp HashFn) Hasher {
+	return Hasher{imp}
+}
+
+func (hr *Hasher) Hash(data ...[]byte) []byte {
+	h := hr.Imp()
+	for _, d := range data {
+		h.Write(d)
+	}
+	return h.Sum(nil)
+}
+
+// --- --- ---
+
+// MerkleTree region
 type MerkleTree struct {
-	RootNode *MerkleNode
+	Root   *MerkleNode
+	Leaves []*MerkleNode
+	hasher Hasher
 }
 
 type MerkleNode struct {
-	Left  *MerkleNode
-	Right *MerkleNode
-	Data  []byte
+	Parent *MerkleNode
+	Left   *MerkleNode
+	Right  *MerkleNode
+	Data   []byte
 }
 
-func NewMerkleNode(left, right *MerkleNode, data []byte) *MerkleNode {
+// NewMerkleNode return a new Merkle node
+func NewMerkleNode(left, right *MerkleNode, data []byte, h Hasher) *MerkleNode {
 	node := MerkleNode{}
 
 	if left == nil && right == nil {
-		hash := sha256.Sum256(data)
+		hash := h.Hash(data)
 		node.Data = hash[:]
 	} else {
 		prevHashes := append(left.Data, right.Data...)
-		hash := sha256.Sum256(prevHashes)
+		hash := h.Hash(prevHashes)
 		node.Data = hash[:]
 	}
 
@@ -30,30 +60,115 @@ func NewMerkleNode(left, right *MerkleNode, data []byte) *MerkleNode {
 	return &node
 }
 
-func NewMerkleTree(data [][]byte) *MerkleTree {
-	var nodes []MerkleNode
-
-	if len(data)%2 != 0 {
-		data = append(data, data[len(data)-1])
+// NewMerkleTree return a new Merkle tree built on list of data with hasher function
+func NewMerkleTree(data [][]byte, h Hasher) *MerkleTree {
+	tree := &MerkleTree{
+		Leaves: make([]*MerkleNode, 0, len(data)),
+		hasher: h,
 	}
 
-	for _, dat := range data {
-		node := NewMerkleNode(nil, nil, dat)
-		nodes = append(nodes, *node)
+	// add leaf nodes.
+	for _, d := range data {
+		tree.Leaves = append(tree.Leaves, &MerkleNode{Data: d})
 	}
 
-	for i := 0; i < len(data)/2; i++ {
-		var level []MerkleNode
+	tree.Root = tree.buildRoot()
+	return tree
+}
 
-		for j := 0; j < len(nodes); j += 2 {
-			node := NewMerkleNode(&nodes[j], &nodes[j+1], nil)
-			level = append(level, *node)
+// VerifyProof verifies the integrity of the given value
+func (tree *MerkleTree) VerifyProof(value []byte, proofs [][]byte, idxs []int, h Hasher) bool {
+	prevHash := value
+	for i := 0; i < len(proofs); i++ {
+		if idxs[i] == 0 {
+			prevHash = h.Hash(proofs[i], prevHash)
+		} else {
+			prevHash = h.Hash(prevHash, proofs[i])
+		}
+	}
+
+	return bytes.Equal(tree.Root.Data, prevHash)
+}
+
+// GetProof returns the Merkle path proof to verify the integrity of the given data.
+func (tree *MerkleTree) GetProof(data []byte) ([][]byte, []int, error) {
+	var (
+		path [][]byte
+		idxs []int
+	)
+
+	// find the leaf node for the specific hash with the leaves list in tree.
+	for _, currentNode := range tree.Leaves {
+		if bytes.Equal(currentNode.Data, data) {
+			// after finding the node, using the relationship of the nodes to find path.
+			parent := currentNode.Parent
+			for parent != nil {
+				// if the current node is the left child, then need the right child to calculate the parent hash
+				// for the proof and vice versa.
+				// i.e:
+				// if CurrentNode == Left ; ParentHash = (CurrentNode.Hash, RightChild.Hash)
+				// if CurrentNode == Right ; ParentHash = (LeftChild.Hash, CurrentNode.Hash)
+				// so we have to add the corresponding hash to the path, and in idxs, we save the hash's position 0
+				// for left and 1 for right. In this way, when we want to verify the proof, we can know if
+				// the given hash is the left o right child.
+				if bytes.Equal(currentNode.Data, parent.Left.Data) {
+					path = append(path, parent.Right.Data)
+					idxs = append(idxs, 1)
+				} else {
+					path = append(path, parent.Left.Data)
+					idxs = append(idxs, 0)
+				}
+				// continue the loop
+				currentNode = parent
+				parent = currentNode.Parent
+			}
+			return path, idxs, nil
+		}
+	}
+	return path, idxs, errors.New("hash does not belong to the tree")
+}
+
+// verify by rebuild the tree and compare new tree hash with current hash.
+func (tree *MerkleTree) Verify() bool {
+	if len(tree.Leaves) == 0 || tree.Root == nil {
+		return false
+	}
+
+	cr := tree.buildRoot()
+	return bytes.Equal(tree.Root.Data, cr.Data)
+}
+
+// build tree with appended child leaves
+func (tree *MerkleTree) buildRoot() *MerkleNode {
+	nodes := tree.Leaves
+	// iterating until reach a single node, which will be root.
+	for len(nodes) > 1 {
+		var parents []*MerkleNode
+
+		// handle case number of node odd
+		// duplicate the last node to concatenate it with itself.
+		if len(nodes)%2 != 0 {
+			nodes = append(nodes, nodes[len(nodes)-1])
 		}
 
-		nodes = level
+		// pairing nodes to build a parent from the pair
+		for i := 0; i < len(nodes); i += 2 {
+			n := &MerkleNode{
+				Left:  nodes[i],
+				Right: nodes[i+1],
+
+				// compute the hash of the new node, which will be the combination of its children's hashes.
+				Data: tree.hasher.Hash(nodes[i].Data, nodes[i+1].Data),
+			}
+
+			parents = append(parents, n)
+			nodes[i].Parent, nodes[i+1].Parent = n, n
+		}
+		// once all possible pairs are processed, the parents become the children, start all over again.
+		nodes = parents
 	}
 
-	tree := MerkleTree{&nodes[0]}
-
-	return &tree
+	return nodes[0]
 }
+
+// --- --- ---
